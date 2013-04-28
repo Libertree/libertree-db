8b326f50f2b35ebc6c41de2edd3ab785f1eba2e0
rekado <rekado@elephly.net>
2013-04-28 22:24:28 +0800

    add column to store IV used to encrypt message

--- migrations/2013-04-28-1930-encrypted-direct-messages.sql
+++ migrations/2013-04-28-1930-encrypted-direct-messages.sql
@@ -1,3 +1,7 @@
 ALTER TABLE message_recipients ADD COLUMN symkey VARCHAR(4096);
 COMMENT ON COLUMN message_recipients.symkey IS
 'The symmetric key to decrypt the message, encrypted under the public key of the recipient.';
+
+ALTER TABLE messages ADD COLUMN iv VARCHAR(256);
+COMMENT ON COLUMN messages.iv IS
+'The initialisation vector used to encrypt the message.'
