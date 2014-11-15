b1c1916c552229e5d020f5d7dafe5b355cb69fb7
Pistos <github.pistos@purepistos.net>
2014-11-15 14:14:15 -0500

    Listen for comment deletions and sent NOTIFY to listeners.

--- functions/basic-triggers.sql
+++ functions/basic-triggers.sql
@@ -26,6 +26,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION notify_deleted_comment() RETURNS trigger AS $$
+BEGIN
+    PERFORM pg_notify('comment_deleted', OLD.id::TEXT || ',' || OLD.post_id::TEXT);
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
 
 CREATE TRIGGER after_insert AFTER INSERT ON chat_messages FOR EACH STATEMENT EXECUTE PROCEDURE notify_new_chat_messages();
 CREATE TRIGGER after_insert AFTER INSERT ON comments      FOR EACH STATEMENT EXECUTE PROCEDURE notify_new_comments();
@@ -35,3 +42,8 @@ CREATE TRIGGER after_update_seen
 AFTER UPDATE OF seen ON notifications
 FOR EACH STATEMENT
 EXECUTE PROCEDURE notify_updated_notifications();
+
+CREATE TRIGGER after_delete
+AFTER DELETE ON comments
+FOR EACH ROW
+EXECUTE PROCEDURE notify_deleted_comment();
