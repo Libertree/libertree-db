449c93c4662f28a872c6ad3ab919a87831eb3f2c
Pistos <git dawt pistos aet purepistos dawt net>
2015-07-13 00:08:37 -0400

    Added non-empty constraints on group fields.

--- migrations/2015-07-12-2216-basic-groups.sql
+++ migrations/2015-07-12-2216-basic-groups.sql
@@ -1,7 +1,7 @@
 CREATE TABLE groups (
       id SERIAL PRIMARY KEY
-    , name_display VARCHAR(255) NOT NULL
-    , description VARCHAR(2048) NOT NULL
+    , name_display VARCHAR(255) NOT NULL CONSTRAINT non_empty_name_display CHECK(length(trim(from name_display))>0)
+    , description VARCHAR(2048) NOT NULL CONSTRAINT non_empty_description CHECK(length(trim(from description))>0)
     , admin_member_id INTEGER NOT NULL REFERENCES members(id)
 );
 
